998793e63069c6f25b17f1db97ce0546bf4492d7
despacito <inteli_zq@163.com>
2017-08-17 23:46:36 +0800

    modify

--- test/test.py
+++ test/test.py
@@ -1,30 +1,11 @@
 # -*- coding: utf-8 -*-
-import cx_Oracle
+import os
+import csv
 
-
-def getConnection():
-    # 获取数据库连接
-    # '用户名/密码@数据库地址:端口号/ServiceName'
-    return cx_Oracle.connect('DTSS_DB_USER/DTSS_DB_USER@10.4.124.88:1621/lhytbill')
-    # 或者
-    # return cx_Oracle.connect('DTSS_DB_USER', 'DTSS_DB_USER', '10.4.124.88:1621/lhytbill')
-
-
-conn = getConnection()
-cursor = conn.cursor()
-cursor.execute('SELECT DISTINCT PROGRAM FROM GDI_SI_EPG_HIS_T WHERE ROWNUM<50')
-# results = cursor.fetchall()
-_iter = cursor.__iter__()
-for row in _iter:
-    print(row[0])
-
-
-# row是元组类型
-# for row in results:
-#     print(row[0])
-cursor.close()
-conn.close()
-
-
-strs = 'http://search.cctv.com/search.php?qtext=%s&type=video' % '哈哈'
-print(strs)
+# 在哪个文件夹下调用就显示哪个文件夹
+pwd = os.getcwd()
+# 获取当前文件真正所在的目录
+pwd = os.path.split(os.path.realpath(__file__))[0]
+# 获取当前文件的真是路径包含当前文件的文件名
+pwd = os.path.realpath(__file__)
+print('the path is : ' + pwd)
