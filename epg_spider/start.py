92a2f5b354708952334cd3a97ab07f86fc6ef1dc
despacito <inteli_zq@163.com>
2017-08-17 23:47:29 +0800

    modify

--- epg_spider/start.py
+++ epg_spider/start.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
-from spiders.epg import EPGSpider
-from scrapy.utils.project import get_project_settings
 from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+from epg_spider.spiders.epg_redis import EpgRedisSpider
+from epg_spider.spiders.epg_redis2 import EpgRedisSpider2
 
 settings = get_project_settings()
 crawler = CrawlerProcess(settings)
-crawler.crawl(EPGSpider)
+crawler.crawl(EpgRedisSpider)
+crawler.crawl(EpgRedisSpider2)
 crawler.start()
