62a06c79ce09d2684f6f99342d2d8ee8b760c85d
despacito <inteli_zq@163.com>
2017-08-28 16:54:46 +0800

    modify

--- epg_spider/pipelines.py
+++ epg_spider/pipelines.py
@@ -6,10 +6,8 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import requests
 import scrapy_redis
-from scrapy.utils.misc import load_object
 from scrapy.utils.serialize import ScrapyJSONEncoder
 from twisted.internet.threads import deferToThread
-from urllib3.exceptions import HTTPError
 
 
 class EpgSpiderPipeline(object):
@@ -38,6 +36,7 @@ class ProxyPipeline(object):
 
     @classmethod
     def from_settings(cls, settings):
+        """ cls 代表当前类本身，cls(args*)相当于调用当前类的__init__(args*)方法"""
         params = {
             'server': scrapy_redis.connection.get_redis_from_settings(settings),
         }
@@ -58,9 +57,11 @@ class ProxyPipeline(object):
             """ 延迟大于2秒的代理就算超时 """
             status_code = requests.get('http://search.cctv.com/', proxies=proxies, timeout=2).status_code
         except:
-            print(proxy + ' connected catch Exception')
+            """ I don't want to see the fucking errors """
+            pass
         else:
             if status_code == 200:
+                print(proxy + ' test success')
                 self.server.sadd(key, proxy)
                 return item
 
