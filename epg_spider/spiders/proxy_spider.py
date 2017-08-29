0f2cd640c444a072100ee4fbdec7bc1f273a09d4
despacito <inteli_zq@163.com>
2017-08-29 14:20:26 +0800

    modify

--- epg_spider/spiders/proxy_spider.py
+++ epg_spider/spiders/proxy_spider.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import scrapy_redis
 from scrapy import Request
 
 from epg_spider.items import ProxyAddress
@@ -12,6 +13,11 @@ class ProxySpider(scrapy.Spider):
     # start_urls = ['http://www.xicidaili.com/wt/']
 
     def start_requests(self):
+        # """ 开始爬取新的代理时先清空redis中的代理数据 """
+        # settings = self.settings
+        # server = scrapy_redis.connection.get_redis_from_settings(settings)
+        # server.delete('proxy_set_test')
+        # 爬取前10页代理
         for page in xrange(1, 10):
             url = 'http://www.xicidaili.com/wt/%d' % page
             yield Request(url=url)
